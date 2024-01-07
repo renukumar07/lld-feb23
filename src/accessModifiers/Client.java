68ff3e2eb958f464635674d3308b9628ca5cd3f7
Renu Kumar Chebrolu <renukumar90@gmail.com>
2024-01-07 14:30:08 +0530

    Parameterized and copy constructor

--- src/accessModifiers/Client.java
+++ src/accessModifiers/Client.java
@@ -5,6 +5,7 @@ public class Client {
     public static void main(String[] args) {
         int x = 10;
         Student student = new Student();
+        System.out.println("DEBUG");
         // new keyword is responsible for creating a new object
         // new keyword creates a new memory/object in the HEAP
         // DOT operator
@@ -14,7 +15,19 @@ public class Client {
         student.psp = 90.0; // protected
         student.batchId = 123; //default;
 
+        Student student1 = new Student("Hitesh", 456, 91.0, "IIT");
+        System.out.println("DEBUG");
 
+        Student student2 = new Student("Rajat", 56789);
+        System.out.println("DEBUG");
+        Student student3 = new Student("Nitesh");
 
+        //student and student4, both of these objects will point to the same memory location.
+        Student student4 = student;
+        student4.name = "Rajat";
+        System.out.println("DEBUG");
+
+        Student student5 = new Student(student);
+        System.out.println("DEBUG");
     }
 }
--- src/accessModifiers/Student.java
+++ src/accessModifiers/Student.java
@@ -9,7 +9,39 @@ public class Student {
 
     private String universityName;
 
+
+
     void doSomething(){
         universityName = "Deepak";
+    }
+
+    public Student(){
+        name = "Saptarshi";
+        batchId = 123;
+        psp = 98.0;
+        universityName = "XYZ";
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    //Parameterized constructors.
+    public Student(String studentName, int batch, double marks, String college){
+        name = studentName;
+        batchId = batch;
+        psp = marks;
+        universityName = college;
+    }
+
+    public Student(String name, int batchId){
+        this.name = name;
+        this.batchId = batchId;
+    }
+
+    public Student(Student st){
+        this.name = st.name;
+        this.batchId = st.batchId;
+        this.psp = st.psp;
     }
 }
